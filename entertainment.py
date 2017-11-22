cab23ab48db4f6e04838903ae0397ea46bf77cbd
Yoonha Kang <yoonhas@gmail.com>
2017-11-21 23:57:29 -0600

    readme

--- entertainment.py
+++ entertainment.py
@@ -1,14 +1,16 @@
 import media
 import fresh_tomatoes
 
-old_boy =media.Movie("Old Boy ", "A stroy for toy", "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg", "https://www.youtube.com/watch?v=D89OHw0VsYM", "Park Chan-wook" ,"2003" )
-midnight = media.Movie("Midnight in Paris", "old story", "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg", "https://www.youtube.com/watch?v=dtiklALGz20","Woody Allen","2011"  )
-spider_man =media.Movie("Spider Man", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg", "https://www.youtube.com/watch?v=Fbfe3dH4LDs","Jon Watts","2017"  )
-Thor =media.Movie("Thor", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg", "https://www.youtube.com/watch?v=ue80QwXMRHg","Taika Waititi","2017"  )
-iron_man =media.Movie("Iron Man", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/d/d5/Iron_Man_3_theatrical_poster.jpg", "https://www.youtube.com/watch?v=2CzoSeClcw0","Shane Black","2013"  )
-Captain =media.Movie("Captain America: Civil War", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg", "https://www.youtube.com/watch?v=dKrVegVI0Us","Anthony Russo","2016"  )
 
+#generate instances with name, story, image url, trailer url, director, and opening date)
+old_boy = media.Movie("Old Boy", "A stroy for toy", "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg", "https://www.youtube.com/watch?v=D89OHw0VsYM", "Park Chan-wook", "2003")
+midnight = media.Movie("Midnight in Paris", "old story", "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg", "https://www.youtube.com/watch?v=dtiklALGz20", "Woody Allen", "2011")
+spider_man = media.Movie("Spider Man", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg", "https://www.youtube.com/watch?v=Fbfe3dH4LDs", "Jon Watts", "2017")
+Thor = media.Movie("Thor", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg", "https://www.youtube.com/watch?v=ue80QwXMRHg", "Taika Waititi", "2017")
+iron_man = media.Movie("Iron Man", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/d/d5/Iron_Man_3_theatrical_poster.jpg", "https://www.youtube.com/watch?v=2CzoSeClcw0", "Shane Black", "2013")
+Captain = media.Movie("Captain America: Civil War", "Hero movie", "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg", "https://www.youtube.com/watch?v=dKrVegVI0Us", "Anthony Russo", "2016")
 
-movies =[old_boy, midnight, spider_man, Thor, iron_man, Captain]
+#put instances in a array
+movies = [old_boy, midnight, spider_man, Thor, iron_man, Captain]
 
-fresh_tomatoes.open_movies_page(movies)
+fresh_tomatoes.open_movies_page(movies) # pass movies array to fresh tomato
--- media.py
+++ media.py
@@ -1,15 +1,15 @@
 import webbrowser
+
+
+#Class file that generates an instace with name, story, image, trailer, director, and opeing date
 class Movie():
     def __init__(self, movie_title, storyline, image, trailer, director, date):
-        self.title =movie_title
-        self.storyline =storyline
-        self.poster_image_url=image
-        self.trailer_youtube_url=trailer
-        self.director=director
+        self.title = movie_title
+        self.storyline = storyline
+        self.poster_image_url = image
+        self.trailer_youtube_url = trailer
+        self.director = director
         self.open_date = date
 
     def show_trailer(self):
-        webbrowser.open(self.trailer)
-
-    def show_image(self):
-        webbrowser.open(self.image)
+        webbrowser.open(self.trailer) #open a window for tailer
